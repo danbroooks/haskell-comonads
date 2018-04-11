606c573e81ad985304ebed2e7a040f0fbc34c222
Dan Brooks <dmbrooks@live.co.uk>
2018-04-11 10:03:15 +0100

    Some code

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# OPTIONS_GHC -Wall #-}
+
 module Main where
 
+import Prelude
+import Control.Comonad
+
+data Free f a = Pure a
+              | Bind (f (Free f a))
+
+instance Functor f => Monad (Free f) where
+  return a = Pure a
+  Pure a >>= f = f a
+  Bind m >>= f = Bind (fmap (>>= f) m)
+
+add :: Int -> Int -> Int
+add a b = a + b
+
 main :: IO ()
-main = putStrLn "Run the tests with cabal test"
+main = putStrLn $ show $ add 2 2
